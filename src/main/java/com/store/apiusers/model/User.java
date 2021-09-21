bf71d1189207ffa081a55fc111cc10e9999613e3
Victor Leite <victor_leite@outlook.com.br>
2021-09-20 21:42:26 -0300

    adicionando integração com postgres

--- src/main/java/com/store/apiusers/model/User.java
+++ src/main/java/com/store/apiusers/model/User.java
@@ -1,13 +1,11 @@
 package com.store.apiusers.model;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
